1e5aa67af2033d248a2cd85341d65d6673ed59ef
Данил-1992 <chigay1992@gmail.com>
2026-04-21 17:01:11 +0300

    Fix API baseURL and frontend env variable

--- client/src/shared/api/axiosInstance.ts
+++ client/src/shared/api/axiosInstance.ts
@@ -6,7 +6,7 @@ import type {
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001/api",
   withCredentials: true,
 });
 
